2ebdd8c536931e74fb77f83ac4dcf98b173bb0c0
r4ulcl <raulcalvolaorden@gmail.com>
2024-01-05 15:42:44 +0000

    Delete callback API

--- manager/api/API.go
+++ manager/api/API.go
@@ -6,49 +6,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 
-	globalstructs "github.com/r4ulcl/nTask/globalstructs"
-	"github.com/r4ulcl/nTask/manager/database"
 	"github.com/r4ulcl/nTask/manager/utils"
 )
-
-// @description Handle callback from worker
-// @summary Handle callback from worker
-// @Tags worker
-// @accept application/json
-// @produce application/json
-// @success 200 "OK"
-// @failure 400 {object} globalstructs.Error
-// @failure 403 {object} globalstructs.Error
-// @security ApiKeyAuth
-// @router /callback [post]
-func HandleCallback(w http.ResponseWriter, r *http.Request, config *utils.ManagerConfig, db *sql.DB, verbose, debug bool, wg *sync.WaitGroup) {
-	_, okWorker := r.Context().Value("worker").(string)
-	if !okWorker {
-		if verbose {
-			log.Println("HandleCallback: { \"error\" : \"Unauthorized\" }")
-		}
-		http.Error(w, "{ \"error\" : \"Unauthorized\" }", http.StatusUnauthorized)
-		return
-	}
-
-	var result globalstructs.Task
-	err := json.NewDecoder(r.Body).Decode(&result)
-	if err != nil {
-		http.Error(w, "{ \"error\" : \"Invalid callback body: "+err.Error()+"\"}", http.StatusBadRequest)
-
-		return
-	}
-
-	err = callback(result, config, db, verbose, debug, wg)
-	if err != nil {
-		http.Error(w, "{ \"error\" : \"Error HandleCallback: "+err.Error()+"\"}", http.StatusBadRequest)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-}
 
 // @description Handle status from user
 // @summary Handle status from user
@@ -95,68 +55,4 @@ func HandleStatus(w http.ResponseWriter, r *http.Request, config *utils.ManagerC
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintln(w, string(jsonData))
-}
-
-func callback(result globalstructs.Task, config *utils.ManagerConfig, db *sql.DB, verbose, debug bool, wg *sync.WaitGroup) error {
-
-	if debug {
-		log.Println(result)
-		log.Println("Received result (ID: ", result.ID, " from : ", result.WorkerName, " with command: ", result.Commands)
-	}
-
-	// Update task with the worker one
-	err := database.UpdateTask(db, result, verbose, debug, wg)
-	if err != nil {
-		if verbose {
-			log.Println("HandleCallback { \"error\" : \"Error UpdateTask: " + err.Error() + "\"}")
-		}
-		// Set the task as running if its pending
-		err = database.SetTaskStatus(db, result.ID, "failed", verbose, debug, wg)
-		if err != nil {
-			if verbose {
-				log.Println("HandleCallback { \"error\" : \"Error SetTaskStatus: " + err.Error() + "\"}")
-			}
-			return err
-		}
-		return err
-	}
-
-	// force set task done
-	// Set the task as done
-	err = database.SetTaskStatus(db, result.ID, "done", verbose, debug, wg)
-	if err != nil {
-		if verbose {
-			log.Println("HandleCallback { \"error\" : \"Error SetTaskStatus: " + err.Error() + "\"}")
-		}
-		return err
-	}
-
-	// if callbackURL is not empty send the request to the client
-	if result.CallbackURL != "" {
-		utils.CallbackUserTaskMessage(config, &result, verbose, debug)
-	}
-
-	// if path not empty
-	if config.DiskPath != "" {
-		//get the task from DB to get updated
-		task, err := database.GetTask(db, result.ID, verbose, debug)
-		if err != nil {
-			return err
-		}
-		err = utils.SaveTaskToDisk(task, config.DiskPath, verbose, debug)
-		if err != nil {
-			return err
-		}
-	}
-
-	// Handle the result as needed
-
-	//Add 1 to Iddle thread in worker
-	// add 1 when finish
-	err = database.AddWorkerIddleThreads1(db, result.WorkerName, verbose, debug, wg)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
