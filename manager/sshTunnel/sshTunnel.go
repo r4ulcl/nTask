6bb849a8bc27121a6158f99f54741cc556a6af6a
r4ulcl <raulcalvolaorden@gmail.com>
2024-01-25 23:19:17 +0100

    Update certs example and fix SSH tunnel multiple servers

--- manager/sshTunnel/sshTunnel.go
+++ manager/sshTunnel/sshTunnel.go
@@ -22,87 +22,93 @@ func forwardData(src, dest net.Conn) {
 	dest.Close()
 }
 
-func publicKeyFile(file string) ssh.AuthMethod {
+func publicKeyFile(file string) (ssh.AuthMethod, error) {
 	buffer, err := os.ReadFile(file)
 	if err != nil {
-		return nil
+		return nil, err
 	}
 
 	key, err := ssh.ParsePrivateKey(buffer)
 	if err != nil {
-		return nil
+		return nil, err
 	}
-	return ssh.PublicKeys(key)
+	return ssh.PublicKeys(key), nil
 }
 
 func StartSSH(config *utils.ManagerSSHConfig, portAPI string, verbose, debug bool) {
-	//return nil
 	log.Println("SSH StartSSH")
+
 	for ip, port := range config.IPPort {
-		log.Println("SSH connecction", ip, port)
+		go func(ip, port string) {
+			log.Println("SSH connecction", ip, port)
 
-		// SSH connection configuration
-		sshConfig := &ssh.ClientConfig{
-			User: config.SSHUsername,
-			Auth: []ssh.AuthMethod{
-				// Use the private key for authentication if provided
-				publicKeyFile(config.PrivateKeyPath),
-			},
-			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-		}
-
-		// If a password is provided, add it as an additional authentication method
-		if config.PrivateKeyPassword != "" {
-			sshConfig.Auth = append(sshConfig.Auth, ssh.Password(config.PrivateKeyPassword))
-		}
-
-		if !checkFileExists(config.PrivateKeyPath) {
-			log.Fatal("File ", config.PrivateKeyPath, " not found")
-		}
-
-		// Connect to the SSH server
-		sshClient, err := ssh.Dial("tcp", ip+":"+port, sshConfig)
-		if err != nil {
-			log.Fatalf("Failed to dial: %s", err)
-		}
-
-		// Remote port to forward
-		remoteAddr := "127.0.0.1:" + portAPI
-		// Local address to forward to
-		localAddr := "127.0.0.1:" + portAPI
-
-		if debug {
-			log.Println("SSH remoteAddr", remoteAddr)
-		}
-
-		// Request remote port forwarding
-		remoteListener, err := sshClient.Listen("tcp", remoteAddr)
-		if err != nil {
-			log.Fatalf("Failed to request remote port forwarding: %v", err)
-		}
-		defer remoteListener.Close()
-
-		fmt.Printf("Remote port forwarding %s to %s via SSH...\n", remoteAddr, localAddr)
-
-		for {
-			// Wait for a connection on the remote port
-			remoteConn, err := remoteListener.Accept()
-			if err != nil {
-				log.Fatalf("Failed to accept connection on remote port: %v", err)
+			if !checkFileExists(config.PrivateKeyPath) {
+				log.Fatal("File ", config.PrivateKeyPath, " not found")
 			}
 
-			// Connect to the local server
-			localConn, err := net.Dial("tcp", localAddr)
+			auth, err := publicKeyFile(config.PrivateKeyPath)
 			if err != nil {
-				log.Printf("Failed to connect to local server: %v", err)
-				remoteConn.Close()
-				continue
+				log.Fatal("Error loading file ", config.PrivateKeyPath, err)
 			}
 
-			// Start forwarding data between local and remote connections
-			go forwardData(remoteConn, localConn)
-			go forwardData(localConn, remoteConn)
-		}
+			// SSH connection configuration
+			sshConfig := &ssh.ClientConfig{
+				User: config.SSHUsername,
+				Auth: []ssh.AuthMethod{
+					auth,
+				},
+				HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+			}
+
+			// If a password is provided, add it as an additional authentication method
+			if config.PrivateKeyPassword != "" {
+				sshConfig.Auth = append(sshConfig.Auth, ssh.Password(config.PrivateKeyPassword))
+			}
+
+			// Connect to the SSH server
+			sshClient, err := ssh.Dial("tcp", ip+":"+port, sshConfig)
+			if err != nil {
+				log.Fatalf("Failed to dial: %s", err)
+			}
+
+			// Remote port to forward
+			remoteAddr := "127.0.0.1:" + portAPI
+			// Local address to forward to
+			localAddr := "127.0.0.1:" + portAPI
+
+			if debug {
+				log.Println("SSH remoteAddr", remoteAddr)
+			}
+
+			// Request remote port forwarding
+			remoteListener, err := sshClient.Listen("tcp", remoteAddr)
+			if err != nil {
+				log.Fatalf("Failed to request remote port forwarding: %v", err)
+			}
+			defer remoteListener.Close()
+
+			fmt.Printf("Remote port forwarding %s to %s via SSH...\n", remoteAddr, localAddr)
+
+			for {
+				// Wait for a connection on the remote port
+				remoteConn, err := remoteListener.Accept()
+				if err != nil {
+					log.Fatalf("Failed to accept connection on remote port: %v", err)
+				}
+
+				// Connect to the local server
+				localConn, err := net.Dial("tcp", localAddr)
+				if err != nil {
+					log.Printf("Failed to connect to local server: %v", err)
+					remoteConn.Close()
+					continue
+				}
+
+				// Start forwarding data between local and remote connections
+				go forwardData(remoteConn, localConn)
+				go forwardData(localConn, remoteConn)
+			}
+		}(ip, port)
 	}
 }
 
